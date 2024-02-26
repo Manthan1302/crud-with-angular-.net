b3cff916e2e53e5c2cd8d0de97c9965e24bdc6f8
Manthan Thakkar <Manthant@btl.com>
2024-02-26 12:23:14 +0530

    crud with angular + .net with sql server

---
+++ webApiCrud/webApiCrud/Model/TblDesignation.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace webApiCrud.Model
+{
+    public class TblDesignation
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public string Designation { get; set; }
+
+    }
+}
---
+++ webApiCrud/webApiCrud/Model/TblEmployee.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Common;
+
+namespace webApiCrud.Model
+{
+    public class TblEmployee
+    {
+        [Key][DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [StringLength(150)]
+        public string Name { get; set; }
+        [StringLength(150)]
+        public String LastName { get; set; }
+        [StringLength(250)]
+        public string Email { get; set; }
+        public int Age { get; set; }
+
+        public DateTime Doj { get; set; }
+        [StringLength(20)]
+        public String Gender { get; set; }
+        public int IsMarried { get; set; }
+        public int IsActive { get; set; }
+        public int DesignationId { get; set; }
+        [NotMapped]
+        public string Designation {  get; set; }
+    }
+}
